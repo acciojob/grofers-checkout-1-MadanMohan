da495b668616833d66e40cfca712cb14d6a44366
Madan Mohan Reddy <119772090+1-MadanMohan@users.noreply.github.com>
2025-07-28 14:54:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+const totalDiv = document.createElement("div");
+document.body.appendChild(totalDiv);
+
 const getSum = () => {
 //Add your code here
+	const prices = document.querySelectorAll(".price");
+  let total = 0;
+
+  prices.forEach(p => {
+    total += Number(p.textContent);
+  });
+
+  totalDiv.textContent =  total;
   
 };
 
